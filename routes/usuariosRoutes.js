3fd9d7c7383a4d4ab5e980f7505a6f5fda00ad2f
Natalia Ruchs <88353503+nruchs@users.noreply.github.com>
2024-11-02 02:18:35 -0300

    feat: implement header

--- routes/usuariosRoutes.js
+++ routes/usuariosRoutes.js
@@ -7,17 +7,33 @@ function verificarAutenticacao(req, res, next) {
     if (req.session.usuarioId) {
         next(); // Usuário autenticado, permite o acesso
     } else {
-        res.redirect("/login"); // Redireciona para a página de login
+        res.redirect("/usuarios/login"); // Redireciona para a página de login
     }
 }
 
-router.post("/", usuariosController.criarUsuario);
+// Rota para exibir a página de registro
+router.get("/registrar", (req, res) => {
+    res.render("registrar", { titulo: "Registrar" });
+});
 
+// Rota para exibir a página de login
+router.get("/login", (req, res) => {
+    res.render("login", { titulo: "Login" });
+});
+
+// Rota para registrar novo usuário (POST)
 router.post("/registrar", usuariosController.registrarUsuario);
+
+// Rota para login de usuário (POST)
 router.post("/login", usuariosController.loginUsuario);
+
+// Rota para logout de usuário
 router.get("/logout", verificarAutenticacao, usuariosController.logoutUsuario);
 
+// Rota para exibir perfil do usuário
 router.get("/perfil", verificarAutenticacao, usuariosController.exibirPerfil);
+
+// Rota para atualizar perfil do usuário
 router.post("/perfil", verificarAutenticacao, usuariosController.atualizarPerfil);
 
 module.exports = router;
