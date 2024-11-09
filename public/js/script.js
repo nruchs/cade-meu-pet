735d420044f73edb7f597eae951523206b55e3ed
Natalia Ruchs <88353503+nruchs@users.noreply.github.com>
2024-11-09 17:26:12 -0300

    fix animal card

--- public/js/script.js
+++ public/js/script.js
@@ -11,7 +11,6 @@ async function excluirAnimal(animalId) {
             const data = await response.json();
             alert(data.mensagemSucesso || "Animal excluído com sucesso!");
 
-            // Remove o card do DOM
             const animalCard = document.getElementById(`animal-card-${animalId}`);
             if (animalCard) {
                 animalCard.remove();
@@ -25,3 +24,55 @@ async function excluirAnimal(animalId) {
         alert("Erro ao excluir o animal.");
     }
 }
+
+function limparCamposSeletor(seletor) {
+    const inputs = document.querySelectorAll(
+      `${seletor} input, ${seletor} select, ${seletor} textarea`
+    );
+    inputs.forEach((input) => {
+      if (input.type === "checkbox" || input.type === "radio") {
+        input.checked = false;
+      } else {
+        input.value = "";
+      }
+    });
+  }
+  
+  function exibirCamposEspecificos() {
+    const situacao = document.getElementById("situacao").value;
+  
+    document.getElementById("camposAdocao").style.display = "none";
+    document.getElementById("camposTutor").style.display = "none";
+    document.getElementById("camposPerdido").style.display = "none";
+  
+    if (situacao === "Para Adoção") {
+      document.getElementById("camposAdocao").style.display = "block";
+      limparCamposSeletor("#camposTutor");
+      limparCamposSeletor("#camposPerdido");
+    } else if (situacao === "Procurando Tutor") {
+      document.getElementById("camposTutor").style.display = "block";
+      limparCamposSeletor("#camposAdocao");
+      limparCamposSeletor("#camposPerdido");
+    } else if (situacao === "Perdido") {
+      document.getElementById("camposPerdido").style.display = "block";
+      limparCamposSeletor("#camposAdocao");
+      limparCamposSeletor("#camposTutor");
+    }
+  }
+
+window.onload = exibirCamposEspecificos;
+
+const recompensaInput = document.getElementById('recompensa');
+recompensaInput.addEventListener('input', function (e) {
+    let value = e.target.value.replace(/\D/g, '');
+    value = (parseFloat(value) / 100).toFixed(2);
+    e.target.value = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    }).format(value);
+});
+
+document.getElementById('form-cadastro').addEventListener('submit', function () {
+    const rawValue = recompensaInput.value.replace(/[R$\s.]/g, '').replace(',', '.');
+    recompensaInput.value = rawValue;
+});
